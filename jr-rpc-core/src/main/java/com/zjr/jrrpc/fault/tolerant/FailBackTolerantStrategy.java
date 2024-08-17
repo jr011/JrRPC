3fc7922b2ac254813212526bb127be1e0ce22895
张津瑞 <60546139+jr011@users.noreply.github.com>
2024-08-17 11:10:20 +0800

    Update FailBackTolerantStrategy.java

--- jr-rpc-core/src/main/java/com/zjr/jrrpc/fault/tolerant/FailBackTolerantStrategy.java
+++ jr-rpc-core/src/main/java/com/zjr/jrrpc/fault/tolerant/FailBackTolerantStrategy.java
@@ -3,6 +3,8 @@ package com.zjr.jrrpc.fault.tolerant;
 import com.zjr.jrrpc.model.RpcResponse;
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
@@ -13,8 +15,18 @@ import java.util.Map;
 public class FailBackTolerantStrategy implements TolerantStrategy {
 
     @Override
-    public RpcResponse doTolerant(Map<String, Object> context, Exception e) {
-        // todo 可自行扩展，获取降级的服务并调用
-        return null;
+    public RpcResponse doTolerant(Map<String, Object> context, Exception e) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        String methodName = context.get("methodName").toString();
+        String className = context.get("className").toString();
+        Object[] parameters = (Object[]) context.get("parameters");
+        Class<?>[] parameterTypes = (Class<?>[]) context.get("parameterTypes");
+        String newClassName = className + "Mock";
+        Class<?> aClass = Class.forName(newClassName);
+        Method method = aClass.getMethod(methodName, parameterTypes);
+        Object aInstance = aClass.getDeclaredConstructor().newInstance();
+        Object result = method.invoke(aInstance, parameters);
+        RpcResponse rpcResponse = new RpcResponse();
+        rpcResponse.setData(result);
+        return rpcResponse;
     }
 }
